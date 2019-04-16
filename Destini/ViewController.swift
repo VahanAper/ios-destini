294170c88295140559a38d5e77a6ba912583ade7
Vahan Asryan <vahan.asryan@avetiq.de>
2019-04-16 11:07:50 +0400

    implement logic

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -32,34 +32,111 @@ class ViewController: UIViewController {
     // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var storyTextView: UILabel!
     
-    // TODO Step 5: Initialise instance variables here
-    
-    
-    
+    var storyIndex = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        start()
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
-    
+        if storyIndex == 1 {
+            if sender.tag == 1 {
+                setStory3()
+            } else if sender.tag == 2 {
+                setStory2()
+            }
+        } else if storyIndex == 2 {
+            if sender.tag == 1 {
+                setStory3()
+            } else if sender.tag == 2 {
+                setStory4()
+                hideActionButtons()
+                showResetButton()
+            }
+        } else if storyIndex == 3 {
+            if sender.tag == 1 {
+                setStory6()
+                hideActionButtons()
+                showResetButton()
+            } else if sender.tag == 2 {
+                setStory5()
+                hideActionButtons()
+                showResetButton()
+            }
+        }
     }
     
-
-
-
+    @IBAction func restartPressed(_ sender: UIButton) {
+        start()
+    }
+    
+    func start() {
+        setStory1()
+        
+        showActionButtons()
+        
+        hideResetButton()
+    }
+    
+    func setStory1() {
+        storyIndex = 1
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+    }
+    
+    func setStory2() {
+        storyIndex = 2
+        storyTextView.text = story2
+        topButton.setTitle(answer2a, for: .normal)
+        bottomButton.setTitle(answer2b, for: .normal)
+    }
+    
+    func setStory3() {
+        storyIndex = 3
+        storyTextView.text = story3
+        topButton.setTitle(answer3a, for: .normal)
+        bottomButton.setTitle(answer3b, for: .normal)
+    }
+    
+    func setStory4() {
+        storyIndex = 4
+        storyTextView.text = story4
+    }
+    
+    func setStory5() {
+        storyIndex = 5
+        storyTextView.text = story5
+    }
+    
+    func setStory6() {
+        storyIndex = 6
+        storyTextView.text = story6
+    }
+    
+    func showResetButton() {
+        resetButton.isHidden = false
+    }
+    
+    func hideResetButton() {
+        resetButton.isHidden = true
+    }
+    
+    func showActionButtons() {
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+    }
+    
+    func hideActionButtons() {
+        topButton.isHidden = true
+        bottomButton.isHidden = true
+    }
 }
 
